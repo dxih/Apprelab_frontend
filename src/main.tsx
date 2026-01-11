964d93a0d279e9021acc50c93128aaec8c9ca178
Peter Shreda <ezinwa.ugochukw@gmail.com>
2026-01-10 16:32:19 -0800

    no theme

--- src/main.tsx
+++ src/main.tsx
@@ -3,22 +3,22 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import ErrorBoundary from "./components/common/ErrorBoundary";
-import { ThemeProvider, CssBaseline } from '@mui/material'
+// import { ThemeProvider, CssBaseline } from '@mui/material'
 import ScrollToTop from "./components/common/ScrollToTop";
-import { theme } from './theme'
+// import { theme } from './theme'
 
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
 <React.StrictMode>
-  <ThemeProvider theme={theme}>
-    <CssBaseline />
+  {/* <ThemeProvider theme={theme}>
+    <CssBaseline /> */}
     <ErrorBoundary>
       <BrowserRouter>
         <ScrollToTop />
         <App />
       </BrowserRouter>
     </ErrorBoundary>
-  </ThemeProvider>
+  {/* </ThemeProvider> */}
 </React.StrictMode>
 
 );
