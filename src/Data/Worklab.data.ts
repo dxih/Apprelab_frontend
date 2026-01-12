c0f112a6b7c1e15b290a439d5ab32b81d62db70d
Peter Shreda <ezinwa.ugochukw@gmail.com>
2026-01-13 00:03:21 +0100

    WorkLabs pages and its components hass been completed

---
+++ src/Data/Worklab.data.ts
@@ -0,0 +1,82 @@
+
+import workFrameOppurtunity1 from "../assets/images/components/worklabs/worklab_opportunity/workslab_frame_1.png";
+import workframeOpportunity2 from "../assets/images/components/worklabs/worklab_opportunity/workslab_frame_2.png";
+import workframeOpportunity3 from "../assets/images/components/worklabs/worklab_opportunity/workslab_frame_3.png";
+import workframeOpportunity4 from "../assets/images/components/worklabs/worklab_opportunity/workslab_frame_4.png";
+import type { ProjectItem } from "../types/project";
+
+ const featuredData: ProjectItem[] = [
+  {
+    id: 1,
+    title: "Frontend Developer for E-commerce Website",
+    subtitle: "Re-imagining our customers facing data Analystic dashboard for better",
+    category: "TECH",
+    duration: "6 weeks",
+    agency: "DigiNext",
+    jobType: "Contract",
+    tags: ["Frontend"],
+    tags2: ["Backend"],
+    image: workframeOpportunity4,
+  },
+  {
+    id: 2,
+    title: "Junior Data Analyst Position",
+    subtitle: "Re-imagining our customers facing data Analystic dashboard for better",
+    category: "DESIGN",
+    duration: "6 weeks",
+    agency: "AgriTech",
+    jobType: "Internship",
+    tags: ["UI", "UX"],
+    tags2: ["Backend"],
+    image: workFrameOppurtunity1,
+  },
+  {
+    id: 3,
+    title: "UI/UX Designer for Agricultural Company",
+    subtitle: "Re-imagining our customers facing data Analystic dashboard for better",
+    category: "MARKETING",
+    duration: "6 weeks",
+    agency: "Socially",
+    jobType: "Full-time",
+    tags: ["Social Media"],
+    tags2: ["Backend"],
+    image: workframeOpportunity2,
+  },
+  {
+    id: 4,
+    title: "Social Media Manager Role",
+    subtitle: "Re-imagining our customers facing data Analystic dashboard for better",
+    category: "DATA",
+    duration: "6 weeks",
+    agency: "DataPoint",
+    jobType: "Internship",
+    tags: ["Analysis"],
+    tags2: ["Backend"],
+    image: workframeOpportunity3,
+  },
+  {
+    id: 4,
+    title: "Social Media Manager Role",
+    subtitle: "Re-imagining our customers facing data Analystic dashboard for better",
+    category: "DATA",
+    duration: "6 weeks",
+    agency: "DataPoint",
+    jobType: "Internship",
+    tags: ["Analysis"],
+    tags2: ["Backend"],
+    image: workframeOpportunity3,
+  },
+  {
+    id: 4,
+    title: "Social Media Manager Role",
+    subtitle: "Re-imagining our customers facing data Analystic dashboard for better",
+    category: "DATA",
+    duration: "6 weeks",
+    agency: "DataPoint",
+    jobType: "Internship",
+    tags: ["Analysis"],
+    tags2: ["Backend"],
+    image: workframeOpportunity3,
+  },
+];
+export default featuredData
