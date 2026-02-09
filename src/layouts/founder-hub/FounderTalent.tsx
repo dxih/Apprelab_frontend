097b6aacaec33b512739c6734098b5e7a1311a33
Peter Shreda <ezinwa.ugochukw@gmail.com>
2026-02-09 12:58:47 +0100

    Add Founders dashboard

---
+++ src/layouts/founder-hub/FounderTalent.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function FounderTalent() {
+  return (
+    <div>
+      Founder Talent
+    </div>
+  )
+}
---
+++ src/layouts/founder-hub/FounderWallet.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function FounderWallet() {
+  return (
+    <div>
+      Founder Wallet
+    </div>
+  )
+}
